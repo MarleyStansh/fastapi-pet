3081cb2da85373e2152650abd1c51433a0eb1f10
Goshan57 <485262323@mail.ru>
2025-05-12 17:15:26 +0300

    Experimented with different queries, added requirements.txt

--- main.py
+++ main.py
@@ -1,13 +1,48 @@
 from fastapi import FastAPI
+from pydantic import BaseModel, EmailStr
 
 import uvicorn
 
 app = FastAPI()
 
 
+class CreateUser(BaseModel):
+    email: EmailStr
+
 @app.get("/")
 def root():
     return "Hello World"
+
+@app.get("/hello/")
+def hello(name: str = "world"):
+    name = name.title().strip()
+    return {"message": f"Hello, {name}"}
+
+@app.post("/users/")
+def create_user(user: CreateUser):
+    return {
+        "message": "success",
+        "user": user.email
+    }
+
+@app.get("/items/latest/")
+def get_latest_item():
+    return {"item_id": 0, "name": "latest"}
+
+@app.get("/items/")
+def list_items():
+    return [
+        "item1",
+        "item2",
+        "item3"
+    ]
+
+@app.get("/items/{item_id}/")
+def get_item_by_id(item_id: int):
+    return {
+        "item_id": item_id,
+        "name": "no_name"
+    }
 
 if __name__ == "__main__":
     uvicorn.run("main:app", reload=True)
