99166cc98bc29252dfc38168de6ccbfbb0e46cfc
Goshan57 <485262323@mail.ru>
2025-05-20 21:17:21 +0300

    Trained on JWT Token auth

---
+++ api_v1/demo_auth/demo_jwt_auth.py
@@ -0,0 +1,122 @@
+from jwt.exceptions import InvalidTokenError
+from fastapi import APIRouter, Depends, Form, HTTPException, status
+from fastapi.security import HTTPBearer, HTTPAuthorizationCredentials
+from users.schemas import UserSchema
+from pydantic import BaseModel
+from auth import utils as auth_utils
+
+
+class TokenInfo(BaseModel):
+    access_token: str
+    token_type: str
+
+
+router = APIRouter(prefix="/jwt", tags=["JWT"])
+
+http_bearer = HTTPBearer()
+
+john = UserSchema(
+    username="john",
+    password=auth_utils.hash_password("qwerty"),
+    email="john@example.com",
+)
+
+sam = UserSchema(
+    username="sam",
+    password=auth_utils.hash_password("secret"),
+)
+
+users_db: dict[str, UserSchema] = {
+    "john": john,
+    "sam": sam,
+}
+
+
+def validate_auth_user(
+    username: str = Form(),
+    password: str = Form(),
+):
+    unauthed_exc = HTTPException(
+        status_code=status.HTTP_401_UNAUTHORIZED,
+        detail="Invalid username or password",
+    )
+    if not (user := users_db.get(username)):
+        raise unauthed_exc
+
+    if not auth_utils.validate_password(
+        password=password, hashed_password=user.password
+    ):
+        raise unauthed_exc
+    if not user.active:
+        raise HTTPException(
+            status_code=status.HTTP_403_FORBIDDEN,
+            detail="User is inactive",
+        )
+    return user
+
+
+def get_current_token_payload(
+    creds: HTTPAuthorizationCredentials = Depends(http_bearer),
+) -> UserSchema:
+    token = creds.credentials
+    try:
+        payload = auth_utils.decode_jwt(
+            token=token,
+        )
+    except InvalidTokenError as e:
+        raise HTTPException(
+            status_code=status.HTTP_401_UNAUTHORIZED, detail=f"invalid token error: {e}"
+        )
+    return payload
+
+
+def get_current_auth_user(
+    payload: dict = Depends(get_current_token_payload),
+) -> UserSchema:
+    username: str | None = payload.get("sub")
+    if user := users_db.get(username):
+        return user
+    raise HTTPException(
+        status_code=status.HTTP_401_UNAUTHORIZED,
+        detail="Token is invalid (user is not found)",
+    )
+
+
+def get_current_active_auth_user(
+    user: UserSchema = Depends(get_current_auth_user),
+):
+    if user.active:
+        return user
+    raise HTTPException(
+        status_code=status.HTTP_403_FORBIDDEN,
+        detail="User is not active",
+    )
+
+
+@router.post("/login/", response_model=TokenInfo)
+def auth_user_issue_jwt(user: UserSchema = Depends(validate_auth_user)):
+    jwt_payload = {
+        "sub": user.username,
+        "username": user.username,
+        "email": user.email,
+    }
+    token = auth_utils.encode_jwt(
+        jwt_payload,
+    )
+    return TokenInfo(
+        access_token=token,
+        token_type="Bearer",
+    )
+
+
+@router.get("/users/me/")
+def auth_user_self_check_info(
+    user: UserSchema = Depends(get_current_active_auth_user),
+    payload: dict = Depends(get_current_token_payload),
+):
+    iat = payload.get("iat")
+    return {
+        "username": user.username,
+        "email": user.email,
+        "logged_in_at": iat,
+    }
--- users/schemas.py
+++ users/schemas.py
@@ -1,8 +1,17 @@
-from typing import Annotated
+from typing import Annotated, ClassVar
 from annotated_types import MinLen, MaxLen
-from pydantic import BaseModel, EmailStr
+from pydantic import BaseModel, ConfigDict, EmailStr
 
 
 class CreateUser(BaseModel):
     username: Annotated[str, MinLen(8), MaxLen(30)]
     email: EmailStr
+
+
+class UserSchema(BaseModel):
+    username: str
+    password: bytes
+    email: EmailStr | None = None
+    active: bool = True
+
+    model_config: ClassVar = ConfigDict(strict=True)
